083282794c8285aecb05d9815f14e4a71bbf2ae1
Alex-Bishka <abishka@g.hmc.edu>
2023-09-22 02:40:56 +0100

    js script

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("hello world!")
+// Check if the page is served from GitHub Pages
+if (window.location.hostname.includes('github.io')) {
+    document.write('<base href="/Fantasy/" />');
+} else {
+    document.write('<base href="/" />');
+}
+
+// removing existing event handlers on the menuBtn
+$(document).off('click', '#menuBtn').on('click', '#menuBtn', function () {
+});
+
+// removing existing event handlers on the closeBtn
+$(document).off('click', '#closeBtn').on('click', '#closeBtn', function () {
+});
+
+// Open the menu - attaches even if the buttons aren't loaded into the dom yet
+$(document).on('click', '#menuBtn', function () {
+    document.getElementById('sideMenu').style.width = '250px';
+    document.getElementById('sideMenu').style.zIndex = '25';
+    document.getElementById('menuBtn').style.cursor = "auto"
+});
+
+// Close the menu - attaches even if the buttons aren't loaded into the dom yet
+$(document).on('click', '#closeBtn', function () {
+    document.getElementById('sideMenu').style.width = '0';
+    document.getElementById('menuBtn').style.cursor = "pointer"
+});
+
+// rediction function for specifc blog
+function redirectToBlog(blogName) {
+    // Redirect to the specific blog page
+    window.location.href = "/Blogs/" + blogName;
+}
+
+// Intercept anchor tag clicks to handle /Blogs specially
+document.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        const href = event.target.getAttribute('href');
+        if (href === '/Blogs') {
+            event.preventDefault(); // Prevent the default action
+            redirectToBlogs(); // Call your custom function
+        }
+    }
+});
+
+// Custom function to handle /Blogs
+function redirectToBlogs() {
+    // Your custom logic here, for example:
+    window.location.href = "/blogs.html"; // Navigate to blog.html
+}
