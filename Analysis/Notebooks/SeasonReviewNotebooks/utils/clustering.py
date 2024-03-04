d5f4b10b3056ec1172d4555bef125605c9ddeb08
Alex Bishka <abishka@g.hmc.edu>
2024-03-03 16:06:21 -0800

    restructure of notebooks

--- Analysis/Notebooks/SeasonReviewNotebooks/utils/clustering.py
+++ Analysis/Notebooks/SeasonReviewNotebooks/utils/clustering.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from yellowbrick.cluster import SilhouetteVisualizer
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
@@ -17,8 +18,11 @@ def create_per_df(df, subset_cols, div_col, div_col_name):
     return df, subset_cols
 
 
-def create_subset_df(df, subset_cols, drop_fantasy=True, len_subset_df=30):
+def create_subset_df(df, subset_cols, drop_fantasy=True, len_subset_df=30, div_col=None, div_col_name=None):
     """"""
+    if div_col and div_col_name:
+        df, subset_cols = create_per_df(df.copy(), subset_cols, div_col, div_col_name)
+
     assert("fantasy_points" in subset_cols[0])
     col_to_sort_by = subset_cols[0]
     
@@ -43,7 +47,7 @@ def create_cluster_vectors(df):
     return X, X_scaled
 
 
-def get_candidate_cluster_size(X_scaled):
+def get_candidate_cluster_size_elbow_method(X_scaled):
     """
     TODO: explore alternative for optimal candidate size
     """
@@ -59,6 +63,26 @@ def get_candidate_cluster_size(X_scaled):
     plt.show()
 
     return kmeans
+
+
+def get_candidate_cluster_size_silhouette_method(X_scaled):
+    """"""
+    _, ax = plt.subplots(3, 2, figsize=(15,8))
+    for i in range(2, 8):
+        '''
+        Create KMeans instances for different number of clusters
+        '''
+        km = KMeans(n_clusters=i, init='k-means++', n_init=10, random_state=0)
+        q, mod = divmod(i, 2)
+        '''
+        Create SilhouetteVisualizer instance with KMeans instance
+        Fit the visualizer
+        '''
+
+        visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[q-1][mod])
+        visualizer.fit(X_scaled) 
+
+    return km
 
 
 def create_clusters(n, kmeans, X, X_scaled, df_review, subset_df, subset_cols):
@@ -98,6 +122,8 @@ def create_cluster_plot(df_cluster, cluster_ranking, subset_cols,
 
     df_cluster.set_index('Names', inplace=True)
 
+    df_cluster.sort_values(by="Cluster", inplace=True)
+
     fig = px.scatter_matrix(
         data_frame=df_cluster,
         dimensions=subset_cols,
@@ -134,3 +160,71 @@ def create_cluster_plot(df_cluster, cluster_ranking, subset_cols,
         fig.write_html(save_path)
 
     fig.show()
+
+
+num_2_word = {
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven"
+}
+STARTING_INDENT = 3
+
+
+def indent(num_indents):
+    return "\t" * num_indents
+
+
+def html_breaking_down_clusters(keys):
+    """"""
+    print(f"{indent(STARTING_INDENT)}<div>")
+    
+    print(f"{indent(STARTING_INDENT + 1)}<p class='blog-p-tag'>")
+    print(f"{indent(STARTING_INDENT + 2)}TODO")
+    print(f"{indent(STARTING_INDENT + 1)}</p>")
+    print(f"{indent(STARTING_INDENT + 1)}<div class='iframe-container'>")
+    print(f"{indent(STARTING_INDENT + 2)}<iframe class='large-iframe' data-src='TODO'></iframe>")
+    print(f"{indent(STARTING_INDENT + 1)}</div>")
+    
+    print(f"{indent(STARTING_INDENT + 1)}<p class='blog-p-tag'>")
+    print(f"{indent(STARTING_INDENT + 2)}TODO:")
+    print(f"{indent(STARTING_INDENT + 1)}</p>")
+    print(f"{indent(STARTING_INDENT + 1)}<ul class='bullet-list'>")
+    for key in keys:
+        print(f"{indent(STARTING_INDENT + 2)}<li><b>Cluster {key}:</b> TODO</li>")
+        
+    print(f"{indent(STARTING_INDENT + 1)}</ul>")
+
+
+def create_cluster_html_tiers(df: pd.DataFrame) -> None:
+    cluster_dict = dict()
+    for index, row in df.iterrows():
+        if row.Cluster not in cluster_dict:
+            cluster_dict[row.Cluster] = [index]
+        else:
+            cluster_dict[row.Cluster].append(index)
+            
+    sorted_cluster_dict = dict(sorted(cluster_dict.items()))
+    keys = list(sorted_cluster_dict.keys())
+    
+    
+    html_breaking_down_clusters(keys)
+    print(f"{indent(STARTING_INDENT + 1)}<div style='display: flex; justify-content: space-evenly;''>")
+    for key in keys:
+        names = cluster_dict[key]
+        
+        print(f"{indent(STARTING_INDENT + 2)}<div>")
+        print(f"{indent(STARTING_INDENT + 3)}<b>Cluster {key} ()</b>")
+        print(f"{indent(STARTING_INDENT + 3)}<ul class='bullet-list'>")
+        for name in names:
+            print(f"{indent(STARTING_INDENT + 4)}<li>{name}</li>")
+        print(f"{indent(STARTING_INDENT + 3)}</ul>")
+        print(f"{indent(STARTING_INDENT + 2)}</div>")
+        if key != keys[-1]:
+            print()
+    print(f"{indent(STARTING_INDENT + 1)}</div>")
+    print(f"""{indent(STARTING_INDENT + 1)}<p class="blog-p-tag">
+    {indent(STARTING_INDENT + 2)}TODO
+    {indent(STARTING_INDENT + 1)}</p>""")
+    print(f"{indent(STARTING_INDENT)}</div>")
